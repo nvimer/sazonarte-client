ae32c312efe1371f4fbc0c153c4455a6814b2108
Nicolas Pantoja Diaz <nspantojadiaz@gmail.com>
2025-10-22 10:05:43 -0500

    feat(table-hooks): Add hooks for manage Tables
    - Create useUpdateTable file for update Tables (useMutation)
    - Create useDeleteTable file for delete Tables created previously
      (useMutation)
    - Create useUpdateTableStatus file for update state of tables (useMutation)

---
+++ src/features/tables/hooks/useDeleteTable.ts
@@ -0,0 +1,22 @@
+import { tablesApi } from "@/api";
+import { queryKeys } from "@/lib";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export function useDeleteTable() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (id: number) => {
+      await tablesApi.deleteTable(id);
+      return id;
+    },
+
+    // On success: remove from cache
+    onSuccess: (deletedId) => {
+      queryClient.invalidateQueries({ queryKey: queryKeys.tables.all });
+      queryClient.resetQueries({
+        queryKey: queryKeys.tables.detail(deletedId),
+      });
+    },
+  });
+}
