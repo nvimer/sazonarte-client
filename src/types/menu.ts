301545f499757ff7371f0c2afe82333800bd3ecc
Nicolas Pantoja Diaz <nspantojadiaz@gmail.com>
2025-10-27 09:52:03 -0500

    feat(menu): Add types of menus-module
    - Create Menu.ts file for types of menus with submodules

---
+++ src/types/menu.ts
@@ -0,0 +1,55 @@
+export interface MenuCategory {
+  id: number;
+  name: string;
+  description?: string;
+  order: number;
+  items: MenuItem[];
+  createdAt: string;
+  updatedAt: string;
+  deleted: boolean;
+  deletedAt?: string;
+}
+
+export interface CreateMenuCategoryInput {
+  name: string;
+  description?: string;
+}
+
+export interface UpdateMenuCategoryInput {
+  name?: string;
+  description?: string;
+}
+export interface MenuItem {
+  id: number;
+  categoryId: number;
+  name: string;
+  description?: string;
+  price: string;
+  isExtra: boolean;
+  isAvailable: boolean;
+  imageUrl?: string;
+  createdAt: string;
+  updatedAt: string;
+  deleted: boolean;
+  deletedAt?: string;
+}
+
+export interface CreateMenuItemInput {
+  categoryId: string;
+  name: string;
+  description?: string;
+  price: string;
+  isAvailable: boolean;
+  isExtra: boolean;
+  imageUrl?: string;
+}
+
+export interface UpdateMenuItemInput {
+  categoryId?: string;
+  name?: string;
+  description?: string;
+  price?: string;
+  isAvailable?: boolean;
+  isExtra?: boolean;
+  imageUrl?: string;
+}
