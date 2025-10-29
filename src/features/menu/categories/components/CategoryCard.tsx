1d36308102774d3e88ef24885a4ccb41e6d7ff86
Nicolas Pantoja Diaz <nspantojadiaz@gmail.com>
2025-10-29 10:27:05 -0500

    style(menus-module): Add styles to CategoryCard
    - Use tailwindcss for give a style to CategoryCard

--- src/features/menu/categories/components/CategoryCard.tsx
+++ src/features/menu/categories/components/CategoryCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components";
 import type { MenuCategory } from "@/types";
 
 interface CategoryCardProps {
@@ -6,24 +7,37 @@ interface CategoryCardProps {
     onDelete: (id: number) => void;
 }
 
-export const CategoryCard = ({
+/**
+ * MenuCategory Card component
+ *
+ *  Displays a single category with actions
+ */
+export function CategoryCard({
     category,
     onEdit,
     onDelete,
-}: CategoryCardProps) => {
+}: CategoryCardProps) {
     return (
-        <div>
-            <div>
-                <h3>category.name</h3>
+        <div className="bg-white p-4 rounded-lg shadow-md border border-gray-200">
+            <div className="flex justify-between items-start mb-2">
+                <h3 className="text-lg font-semibold text-gray-800">{category.name}</h3>
             </div>
-            {category.description && <p>category.description</p>}
-
-            <div>
-                <Button>Editar</Button>
-            </div>
-            <div>
-                <Button>Eliminar</Button>
+            {category.description && (
+                <p className="text-gray-600 text-sm mb-4">{category.description}</p>
+            )}
+            <p className="text-gray-600 text-sm mb-4">{category.order}</p>
+            <div className="flex gap-2">
+                <Button variant="primary" size="sm" onClick={() => onEdit(category)}>
+                    Editar
+                </Button>
+                <Button
+                    variant="danger"
+                    size="sm"
+                    onClick={() => onDelete(category.id)}
+                >
+                    Eliminar
+                </Button>
             </div>
         </div>
     );
-};
+}
