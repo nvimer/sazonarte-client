1df209db9d43cfca0b12784fd319efbbebeaa2b3
Nicolas Pantoja Diaz <nspantojadiaz@gmail.com>
2025-11-07 20:12:04 -0500

    feat(landing-page): Add a landing-page to project
    - Create landing-page file to desing the component to sell our product.
    - Use the navbar component created previously

---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,28 @@
+import { Navbar } from "../sections";
+
+/**
+ * LandingPage Component
+ *
+ * Main landing page combining all sections
+ *
+ * Structure /
+ * - Navbar (sticky)
+ * - Hero Section
+ * - Features Section
+ * - Stats Section
+ * - CTA Section
+ * - Footer
+ */
+export function LandingPage() {
+  return (
+    <div>
+      {/* ================= NAVIGATION ================ */}
+      <Navbar />
+
+      {/* ============== MAIN CONTENT ================= */}
+      <main></main>
+
+      {/* =========== FOOTER ===========  */}
+    </div>
+  );
+}
